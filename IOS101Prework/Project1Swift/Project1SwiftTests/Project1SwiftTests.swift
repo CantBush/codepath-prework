b589f41329588d4dc37c158c75bcc5a8bd9d5ec6
CantBush <an8493574@gmail.com>
2024-12-15 19:28:33 -0800

    Added prework files to repository

---
+++ IOS101Prework/Project1Swift/Project1SwiftTests/Project1SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Project1SwiftTests.swift
+//  Project1SwiftTests
+//
+//  Created by Andrew Nguyen on 12/15/24.
+//
+
+import Testing
+@testable import Project1Swift
+
+struct Project1SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
